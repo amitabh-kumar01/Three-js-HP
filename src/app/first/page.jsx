f03f0f2903a321772d300ba3bb1c2efc6e281acd
Amitabh kumar <amitabhkumar01.hp@outlook.com>
2024-10-04 12:27:54 +0530

    inital commit

---
+++ src/app/first/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { First } from '../../components';
+const page = () => {
+  return (
+
+    <First/>
+  );
+}
+
+export default page
---
+++ src/app/second/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Second } from '../../components'
+
+const page = () => {
+  return (
+    <>
+    <Second/>
+    </>
+  )
+}
+
+export default page
